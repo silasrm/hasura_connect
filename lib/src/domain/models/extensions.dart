b232522c0d0cb437559a2b186f64080d977a1cb6
Silas Ribas Martins <silasrm@gmail.com>
2020-08-19 01:25:43 -0300

    Add support to validation property

--- lib/src/domain/models/extensions.dart
+++ lib/src/domain/models/extensions.dart
@@ -1,10 +1,11 @@
 class Extensions {
   final dynamic path;
   final dynamic code;
+  final dynamic validation;
 
-  Extensions(this.path, this.code);
+  Extensions(this.path, this.code, this.validation);
 
   factory Extensions.fromJson(Map json) {
-    return Extensions(json['path'], json['code']);
+    return Extensions(json['path'], json['code'], json['validation']);
   }
 }
